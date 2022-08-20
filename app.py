dc6935fe8d836d78b8d70c4dda627f78f9fd7b2e
Mogens Hobolth <mhobolth@gmail.com>
2022-08-20 16:17:25 +0200

    First version of webapp and data fetcher

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, session
+from datetime import datetime
+import pandas as pd
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\asd]/'
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    df = pd.read_csv('static/data_sample.csv')
+    id = '1'
+    navid = 1
+    navid_prev = 1
+    navid_next = 1
+    vote = 4
+    votes = {}
+    rowno = 0
+    no_votes = len(df.index)
+    if request.method == 'POST':
+        votes = session['votes']
+        id = str(request.form['id'])
+        vote = int(request.form['vote'])
+        navid = int(request.form['navid'])
+        rowno = df[df["id"] == navid].iloc[0].raekkenr
+        if rowno == 0:
+            navid_prev = navid
+        else:
+            navid_prev = df[df["raekkenr"] == rowno-1].iloc[0].id #fix edge case (first row)
+        if rowno+1 == len(df.index):
+            navid_next = navid
+        else:
+            navid_next = df[df["raekkenr"] == rowno+1].iloc[0].id #fix edge case (last row)
+    else:
+        id = str(df.iloc[0].id)
+        navid = int(id)
+        navid_prev = navid
+        navid_next = df.iloc[1].id
+    votes[id] = vote
+    session['votes'] = votes
+    id = navid
+    progress_bar = int(rowno/no_votes * 100)
+    if str(navid) in votes:
+        vote = votes[str(navid)]
+    else:
+        vote = 4
+    row = df[df["id"] == navid].head(1).iloc[0]
+    similarity_ALT = calculateSimilarity(votes, df, 'ALT')
+    similarity_EL = calculateSimilarity(votes, df, 'EL')
+    similarity_SF = calculateSimilarity(votes, df, 'SF')
+    similarity_RV = calculateSimilarity(votes, df, 'RV')
+    similarity_S = calculateSimilarity(votes, df, 'S')
+    similarity_V = calculateSimilarity(votes, df, 'V')
+    similarity_KF = calculateSimilarity(votes, df, 'KF')
+    similarity_DF = calculateSimilarity(votes, df, 'DF')
+    return render_template(
+        "partitest.html",
+        vote=vote,
+        id=id,
+        ministeromraade=row.ministeromraade,
+        titel = row.titel,
+        resume = row.resume,
+        navid_prev = navid_prev,
+        navid_next = navid_next,
+        date=datetime.now(),
+        progress_bar=progress_bar,
+        partier = ['ALT','EL', 'SF', 'RV', 'S', 'V', 'KF','DF'],
+        partienighed = [similarity_ALT, similarity_EL, similarity_SF, similarity_RV, similarity_S, similarity_V, similarity_KF, similarity_DF]
+    )
+
+def calculateSimilarity(votes, df, party):
+    total = 0
+    count_similar = 0
+    result = 0
+    for k, v in votes.items():
+        if v == 4:
+            continue
+        row = df[df["id"] == int(k)]
+        if row['stem_' + party].iloc[0] == v:
+            count_similar += 1
+        total += 1
+    if total > 0:
+        result = int(count_similar / total * 100)
+    return result
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Partitest
+
+Ved afholdelse af valg i Danmark bliver der ofte på de større nyhedssites lanceret forskellige tests, hvor man gennem en række spørgsmål kan få vist, hvor enig/uenig man er med feltet af kandidater. Til folketingsvalget i 2019 udarbejdede f.x. dr.dk endvidere end partitest, hvor man ved at tage stilling til en række "markante" lovforslag fik vist, hvilket parti der havde en afstemningshistorik tættest på en selv. 
+
+Formålet med dette projekt er at stille en partitest til offentlig rådighed, som er baseret på en automatisk indhentning af lovforslagsdata fra ft.dk og en statistisk/analytisk model til at automatisk det bedste og færreste sæt af spørgsmål til at måle egne præferencer op imod partiernes afslørede holdninger. Det er i den sammenhæng en vigtig pointe, at partiernes afstemningsadfærd ikke alene afspejsler partipolitiske holdninger men også parliamentarisk taktisk adfærd.
+
+Den data og analytiske side af projektet består af følgende filer: 
+
+* get_data_from_ft: Python notebook som henter lovforslagsdata fra ft.dk og opbygger et datasæt, som kan bruges til udvælge spørgsmål til testen.
+* explore_data: Python notebook med deskriptiv statistik om de indhentede lovforslag
+* sample_test_questions: Python notebook med logikken for udvælgelse af spørgsmål til testen
+
+Selve partitesten er lavet som en hjemmeside og består af følgende filer: 
+
+* app.py: Backend kode, som sørger for at servicere brugeren med spørgsmål og beregne stemmeligheden med de forskellige partier ud fra de afgivne svar. 
+* templates/partitest.html: Frontend kode med selve brugergrænsefladen
